02355cf43e347df8cf8aa0b69927b994edbf2b39
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-07-07 20:57:35 +0530

    adding kafka Producer configurations

--- Event_Producer/.idea/sonarlint/securityhotspotstore/index.pb
+++ Event_Producer/.idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@ T
 $src/main/java/edu/icet/crm/Main.java,a\7\a79c7638a616e1f0dfb0b7452f97745ba1783a6a
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+j
+:src/main/java/edu/icet/crm/config/KafkaProducerConfig.java,3\d\3d92f95da3b45a8a29a6692e7647a90f6632fd55
---
+++ Event_Producer/src/main/java/edu/icet/crm/config/KafkaProducerConfig.java
@@ -0,0 +1,32 @@
+package edu.icet.crm.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-server}")
+    private String kafkaServerUrl;
+@Bean
+    public ProducerFactory<String, String> productFactory() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl); //setting url
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);// convert the data as packet
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);//need to serialize the java data type to packet data
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+@Bean
+    public KafkaTemplate<String,String> kafkaTemplate(){
+        return new KafkaTemplate<>(productFactory()); // creating kafka template to connect the kafka server
+    }
+}
